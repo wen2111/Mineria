803d0d595bf59ec1e66e21320a3ab3edb9885771
wen2111 <wenjia.ye@estudiantat.upc.edu>
2025-09-30 23:20:44 +0200

    data

---
+++ Entrega_1/AED.R
@@ -0,0 +1,12 @@
+# install.packages("psych")
+# install.packages("dlookr")
+
+## ==== AED ====
+# install.packages("skimr")
+# install.packages("tidyverse")
+library(skimr)
+library(tidyverse)
+
+## Podem visualitzar un descriptiu de les dades 
+skim(data)
+
